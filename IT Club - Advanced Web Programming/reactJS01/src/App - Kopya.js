00a93e12b41429d8ce1ba2617b670f86da3b1fbe
Numir <numirselman@gmail.com>
2020-06-04 00:10:57 +0200

    Create App - Kopya.js

---
+++ IT Club - Advanced Web Programming/reactJS01/src/App - Kopya.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { InputList } from './input';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Merhaba SüleyMAN </h3> <h2> React App Denemesi </h2>
+        <hr /> <br />
+        <InputList> </InputList>
+      </div>
+    );
+  }
+}
+export default App;
